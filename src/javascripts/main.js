d9bf80e844fbfe957a0e06e0f4aead916b421a01
Logan-Watt <loganwatt@mail.weber.edu>
2024-03-14 20:32:47 -0600

    beginning practice with javascript

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,12 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+for(let i = 0; i < 10; i++) {
+    let r = Math.random()
+    if(r < .5) {
+        document.write('<div class="red box"></div>')
+    }
+    else {
+        document.write('<div class="blue box"></div>')
+    }
+}
